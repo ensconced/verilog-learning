52a2158e590a65d73e7fff8375ee07b9f362b9ef
Joe Barnett <joe.barnett@elliptic.co>
2024-02-16 19:45:44 +0000

    wip

--- build.tcl
+++ build.tcl
@@ -4,21 +4,22 @@ foreach source_file [glob *.v] {
 
 read_xdc basys3.xdc
 
-# set simset_list {}
+set simset_list {}
 
-# foreach test_file [glob test/ram16k_tb.vhd] {
-#   set base_name [file rootname [file tail $test_file]]
-#   set new_simset [create_fileset -simset $base_name]
-#   add_files -fileset $base_name $test_file
-#   set_property top $base_name $new_simset
-#   lappend simset_list $new_simset
-# }
+foreach test_file [glob *_tb.v] {
+  set base_name [file rootname [file tail $test_file]]
+  set new_simset [create_fileset -simset $base_name]
+  add_files -fileset $base_name $test_file
+  set_property top $base_name $new_simset
+  lappend simset_list $new_simset
+}
 
 save_project_as -force verilog-learning
 
-# foreach simset $simset_list {
-#   launch_simulation -simset $simset
-# }
+
+foreach simset $simset_list {
+  launch_simulation -simset $simset
+}
 
 synth_design -top top -part xc7a35tcpg236-1
 opt_design
