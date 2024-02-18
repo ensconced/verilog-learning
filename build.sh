087281ee97abf6204785c4fbb653849e07b06403
Joe Barnett <joe.barnett@elliptic.co>
2024-02-18 15:00:05 +0000

    wip

--- build.sh
+++ build.sh
@@ -2,14 +2,7 @@
 
 set -eux
 
-echo "### COMPILING SYSTEMVERILOG ###"
-xvlog --sv --incr --relax ./src/adder.sv ./src/tb.sv
-
-echo "### ELABORATING ###"
+xvlog --sv --incr --relax ./projects/switch/top.v ./projects/switch/tb.sv
 xelab -debug all -top tb -snapshot adder_tb_snapshot
-
-echo "### RUNNING SIMULATION ###"
 xsim adder_tb_snapshot --tclbatch xsim_cfg.tcl
-
-echo "### OPENING WAVES ###"
 xsim --gui adder_tb_snapshot.wdb
